cb8362a242d0f5f2f9b54231398648d2c2249d60
khadijaDev <khadijaoukhlift@gmail.com>
2025-03-03 00:16:53 +0100

    Logout servlet

--- src/main/java/com/Controller/Logout.java
+++ src/main/java/com/Controller/Logout.java
@@ -1,4 +1,24 @@
 package com.Controller;
 
-public class Logout {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LogoutServlet")
+public class Logout extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false); // Récupérer la session existante (sans en créer une nouvelle)
+        if (session != null) {
+            session.invalidate(); // Supprimer la session
+        }
+        response.sendRedirect("home.jsp"); // Redirection vers la page d'accueil
+    }
 }
